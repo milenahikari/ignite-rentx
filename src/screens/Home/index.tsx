072ed341d6b926114bc9a2368422a20b08668f92
Milena Hikari <milenahikari11@gmail.com>
2021-07-18 17:41:03 -0300

    Criação do Header da Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+import Logo from "../../assets/logo.svg";
 
 import * as S from "./styles";
 
 const Home: React.FC = () => {
   return (
     <S.Container>
-      <S.Title>Home</S.Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <S.Header>
+        <S.HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+
+          <S.TotalCars>Total de 12 carros</S.TotalCars>
+        </S.HeaderContent>
+      </S.Header>
     </S.Container>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,16 +3,30 @@ import styled, { css } from "styled-components/native";
 export const Container = styled.View`
   ${({ theme }) => css`
     flex: 1;
-    justify-content: center;
-    align-items: center;
-
     background-color: ${theme.colors.background_primary};
   `}
 `;
 
-export const Title = styled.Text`
+export const Header = styled.View`
   ${({ theme }) => css`
-    font-size: 30px;
-    font-family: ${theme.fonts.secondary_600};
+    width: 100%;
+    height: 113px;
+    background-color: ${theme.colors.header};
+    justify-content: flex-end;
+    padding: 32px 24px;
+  `}
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const TotalCars = styled.Text`
+  ${({ theme }) => css`
+    font-size: 15px;
+    font-family: ${theme.fonts.primary_400};
+    color: ${theme.colors.text};
   `}
 `;
