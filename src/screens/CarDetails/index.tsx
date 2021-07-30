25e56119c62790283bb2768f5fc1d35d255ef6ba
Milena Hikari <milenahikari11@gmail.com>
2021-07-30 17:23:30 -0300

    Desenvolvendo componente de acessorios para os carros

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -2,6 +2,14 @@ import React from "react";
 
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/ImageSlider";
+import { Accessory } from "../../components/Accessory";
+
+import speedSvg from "../../assets/speed.svg";
+import accelerationSvg from "../../assets/acceleration.svg";
+import forceSvg from "../../assets/force.svg";
+import gasolineSvg from "../../assets/gasoline.svg";
+import exchangeSvg from "../../assets/exchange.svg";
+import peopleSvg from "../../assets/people.svg";
 
 import * as S from "./styles";
 
@@ -33,6 +41,14 @@ export function CarDetails() {
           </S.Rent>
         </S.Details>
 
+        <S.Accessories>
+          <Accessory name="380Km/h" icon={speedSvg} />
+          <Accessory name="3.2s" icon={accelerationSvg} />
+          <Accessory name="800 HP" icon={forceSvg} />
+          <Accessory name="Gasolina" icon={gasolineSvg} />
+          <Accessory name="Auto" icon={exchangeSvg} />
+          <Accessory name="2 pessoas" icon={peopleSvg} />
+        </S.Accessories>
         <S.About>
           Este é automóvel desportivo. Surgiu do lendário touro de lide
           indultado na praça Real Maestranza de Sevilla. É um belíssimo carro
--- src/screens/CarDetails/styles.ts
+++ src/screens/CarDetails/styles.ts
@@ -81,12 +81,23 @@ export const Price = styled.Text`
   `}
 `;
 
+export const Accessories = styled.View`
+  width: 100%;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: 16px;
+`;
+
 export const About = styled.Text`
   ${({ theme }) => css`
     font-family: ${theme.fonts.primary_400};
     color: ${theme.colors.text};
     font-size: ${RFValue(15)}px;
     text-align: justify;
+    line-height: ${RFValue(25)}px;
 
     margin-top: 23px;
   `}
